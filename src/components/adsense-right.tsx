daffe32ff0480f46ee5b90f7cd9a8f57435c3d84
Gabriel Grozinski <gabrielgrozinski@gmail.com>
2026-03-03 16:34:19 -0300

    confere-ae-0.95.8

--- src/components/adsense-right.tsx
+++ src/components/adsense-right.tsx
@@ -7,12 +7,12 @@ export default function AdsenseRight() {
 
     return (
         <>
-        {assinanteAtual !== '' ?
-        <span className='min-w-2 max-w-2 sticky left-0 top-25 hidden lg:flex'></span>
-        :
+        {(assinanteAtual === 'Sócio' || assinanteAtual === 'Torcedor') ?
         <div className="lg:flex justify-end hidden sticky left-0 top-25 self-start">
             <img className="max-w-[90%]" src={adsense} alt="" />
         </div>
+        :
+        <span className='min-w-2 max-w-2 sticky left-0 top-25 hidden lg:flex'></span>
         }
         </>
     )
